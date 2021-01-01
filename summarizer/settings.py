786927a449a920c28b2f76bd7d77d730597e293d
Tejas Arlimatti <tejasarlimatti@gmail.com>
2021-01-01 18:58:39 +0530

    Added settings for collecting static files

--- summarizer/settings.py
+++ summarizer/settings.py
@@ -17,6 +17,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
